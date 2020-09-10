cce31c63336a234c48b2907d4f9bf0df0db0aafc
ronbravo <ronbravo1701@gmail.com>
2020-09-10 11:43:33 -0700

    Setting up the project with a basic HTTP server.

---
+++ app/README.md
@@ -0,0 +1,3 @@
+# App
+
+Here is where all the app repo branches are placed as to help manage and organize the project better. 
---
+++ pm2.config.orig.js
@@ -0,0 +1,17 @@
+const { resolve } = require('path');
+const homedir = require('os').homedir();
+
+// NOTE: Replace this path with your projects are.
+const workspace = resolve(homedir, 'workspace', 'dev');
+
+module.exports = {
+  apps: [
+    // {
+    //   namespace: 'sample-proj',     // a group name for related apps
+    //   name: 'sp-dev',               // app / service name
+    //   cwd: `${workspace}/dev/cli`,  // directory to start app from
+    //   script: 'npm',                // The script start command
+    //   args: 'run dev'               // params to give the command
+    // }
+  ]
+}
