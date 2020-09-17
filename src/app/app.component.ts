531513052510f8416ceee71adc6a6801386d9bbf
Ronald Bravo <ronald.bravo@experian.com>
2020-09-16 23:54:28 -0700

    WIP: Save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ng-app-root',
+  selector: 'experian-eds-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
